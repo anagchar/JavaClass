b7ac558e22c3bc6a04df0c12914739df4f12a82c
Charalampos Anagnostakis <charalampos.anagnostakis@student.kuleuven.be>
2024-05-10 12:49:23 +0200

    minor changes

--- students/src/students/Student.java
+++ students/src/students/Student.java
@@ -9,7 +9,45 @@ package students;
  */
 public class Student {
 	
+	/**
+	 * @invar The biderictional association between a Student and the teammate object is consistent. 
+	 * 		| teammate == null || teammate.teammate == this
+	 * @peerObject
+	 */
+	private Student teammate;
+	
+	/**
+	 * @peerObject
+	 */
 	public Student getTeammate() {
+		throw new RuntimeException("Not yet implemented");
+	}
+	
+	/**
+	 * @post | getTeammate() == null
+	 */
+	public Student() {
+		throw new RuntimeException("Not yet implemented");
+	}
+	
+	/**
+	 * @pre | getTeammate() != null
+	 * @pre | other.getTeammate() == null
+	 * @pre | other != null
+	 * @mutates | this, other
+	 * @post | getTeammate() == other
+	 */
+	public void setTeammate(Student other) {
+		throw new RuntimeException("Not yet implemented");
+	}
+	
+	/**
+	 * @pre | getTeammate() != null
+	 * @mutates | this
+	 * @post | getTeammate() == null
+	 * @post | old(getTeammate()).getTeammate() == null
+	 */
+	public void clearTeammate() {
 		throw new RuntimeException("Not yet implemented");
 	}
 
