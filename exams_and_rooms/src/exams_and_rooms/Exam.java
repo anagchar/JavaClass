aed9e27f8559e49c80bcab4b64a385005d49c10e
Charalampos Anagnostakis <charalampos.anagnostakis@student.kuleuven.be>
2024-05-24 11:44:49 +0200

    minor changes

--- exams_and_rooms/src/exams_and_rooms/Exam.java
+++ exams_and_rooms/src/exams_and_rooms/Exam.java
@@ -2,11 +2,18 @@ package exams_and_rooms;
 
 import java.util.Set;
 
+
+/**
+ * 
+ * 
+ * @invar | getRooms().stream().allMatch(room -> room != null && room.getExams().contains(this))
+ */
 public class Exam {
 	
 	/**
 	 * @creates | result
 	 * @peerObjects
+	 * @post | result != null
 	 */
 	public Set<Room> getRooms() { throw new RuntimeException("Not implemented yet."); }
 }
--- exams_and_rooms/src/exams_and_rooms/Room.java
+++ exams_and_rooms/src/exams_and_rooms/Room.java
@@ -1,5 +1,9 @@
 package exams_and_rooms;
 
+import java.util.Set;
+
 public class Room {
+	
+	public Set<Exam> getExams() { throw new RuntimeException("Not implemented yet."); }
 
 }
