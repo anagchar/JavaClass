4dfdffe228e94209fb6e6090ed79159f99d788b1
Charalampos Anagnostakis <charalampos.anagnostakis@student.kuleuven.be>
2024-05-29 13:29:39 +0200

    Fully implemented with JUnit test

--- Prep_for_exams/src/persons/Person.java
+++ Prep_for_exams/src/persons/Person.java
@@ -3,18 +3,44 @@ package persons;
 import java.util.HashSet;
 import java.util.Set;
 
+import logicalcollections.LogicalSet;
+
+/**
+ * @invar | getFather() == null || getFather().getChildren().contains(this)
+ * @invar | getChildren().stream().allMatch(child -> child != null && child.getFather() == this)
+ */
 public class Person {
 	
+	/**
+	 * @peerObject
+	 * @invar | father == null || father.children.contains(this)
+	 */
 	private Person father;
+	
+	/**
+	 * @peerObjects
+	 * @representationObjects
+	 * @invar | children != null
+	 * @invar | children.stream().allMatch(child -> child != null && child.father == this)
+	 */
 	private Set<Person> children;
 	
+	/**
+	 * @post | getFather() == null
+	 * @post | getChildren().isEmpty()
+	 */
 	public Person() {
 		this.children = new HashSet<>();
 	}
 	
 	/**
 	 * @pre | father != null
+	 * @pre | getFather() == null
+	 * @mutates | this, father
 	 * @mutates_properties | father.getChildren(), this.getFather()
+	 * @post | getFather() == father
+	 * @post "father"'s set of childern equals their old set of children plus "this".
+	 * 		 | father.getChildren().equals(LogicalSet.plus(old(father.getChildren()), this)) 
 	 */
 	public void setFather(Person father) { 
 		if (this.father != null) {
@@ -24,15 +50,33 @@ public class Person {
 		father.children.add(this); // Bidirectional association
 	}
 	
+	
+	/**
+	 * @pre | getFather() != null
+	 * @mutates_properties | this.getFather(), this.getFather().getChildren()
+	 * @post | getFather() == null
+	 * @post "father"'s set of childern equals their old set of children minus "this".
+	 * 		| old(getFather()).getChildren().equals(LogicalSet.minus(old(getFather().getChildren()), this)) 
+	 */
 	public void removeFather() { 
-		this.father = null;
 		father.children.remove(this);
+		this.father = null;
 	}
 	
+	/**
+	 * @peerObject
+	 * @inspects | this
+	 */
 	public Person getFather() { 
 		return this.father;
 	}
 	
+	/**
+	 * @peerObjects
+	 * @creates | result
+	 * @post | result != null
+	 * @post | result.stream().allMatch(child -> child != null)
+	 */
 	public Set<Person> getChildren() { 
 		return Set.copyOf(children);
 	}
---
+++ Prep_for_exams/src/persons/PersonTest.java
@@ -0,0 +1,32 @@
+package persons;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+class PersonTest {
+
+	@Test
+	void test() {
+		Person albert = new Person();
+		assertNull(albert.getFather());
+		assertEquals(Set.of(), albert.getChildren());
+		
+		Person philippe = new Person();
+		philippe.setFather(albert);
+		assertSame(albert, philippe.getFather());
+		assertEquals(Set.of(philippe), albert.getChildren());
+		
+		Person laurent = new Person();
+		laurent.setFather(albert);
+		assertEquals(Set.of(philippe, laurent), albert.getChildren());
+		
+		laurent.removeFather();
+		assertNull(laurent.getFather());
+		assertEquals(Set.of(philippe), albert.getChildren());
+		 
+	}
+
+}
