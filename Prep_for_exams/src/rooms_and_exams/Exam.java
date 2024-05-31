04ad1657b895da739454738b2203c6eb654cffc6
Charalampos Anagnostakis <charalampos.anagnostakis@student.kuleuven.be>
2024-05-31 15:58:04 +0200

    Full implementation for Room and Exam

--- Prep_for_exams/src/rooms_and_exams/Exam.java
+++ Prep_for_exams/src/rooms_and_exams/Exam.java
@@ -3,16 +3,41 @@ package rooms_and_exams;
 import java.util.HashSet;
 import java.util.Set;
 
+import logicalcollections.LogicalSet;
+
+/**
+ * @invar | getRooms().stream().allMatch(room -> room != null && room.getExams().contains(this))
+ * @invar | getRooms() != null
+ */
 public class Exam {
 	
+	/**
+	 * @invar | exam != null
+	 * @invar | rooms.stream().allMatch(room -> room != null && room.getExams().contains(this))
+	 */
 	private Exam exam;
+	/**
+	 * 
+	 * @peerObjects
+	 */
 	private Set<Room> rooms;
 	
+	/**
+	 * @creates | result
+	 * @post | result != null
+	 * 
+	 */
 	public Set<Room> getRooms() { 
 		return new HashSet<>(rooms); // to avoid representation exposure
 	}
 	
-	public Set<Room> addRoom(Room room) { 
+	/**
+	 * @pre | getRooms() != null
+	 * @pre | room != null
+	 * @mutates_properties | this.getRooms(), room.getExams() 
+	 * @post | room.getExams().equals(LogicalSet.plus(old(room.getExams()), this))
+	 */
+	public Set<Room> addRoom(Room room) {
 		if (rooms.contains(room)) {
 			return new HashSet<>(rooms);
 		} else {
@@ -22,6 +47,12 @@ public class Exam {
 		}
 	}
 	
+	/**
+	 * @pre | getRooms() != null
+	 * @pre | room != null
+	 * @mutates_properties | this.getRooms(), room.getExams()
+	 * @post | room.getExams().equals(LogicalSet.minus(old(room.getExams()), this))
+	 */
 	public Set<Room> removeRoom(Room room) { 
 		if (rooms.contains(room)) {
 			rooms.remove(room);
@@ -31,11 +62,19 @@ public class Exam {
 		return new HashSet<>(null);
 	}
 	
+	/**
+	 * @pre | name != null
+	 * @creates | this
+	 */
 	public Exam(String name) { 
 		name = exam.toString();
 		this.rooms = new HashSet<>();
 	}
 	
+	/**
+	 * @post | result != null
+	 * @inspects | this
+	 */
 	public Exam getExam() { 
 		return this.exam;
 	}
--- Prep_for_exams/src/rooms_and_exams/Room.java
+++ Prep_for_exams/src/rooms_and_exams/Room.java
@@ -3,15 +3,40 @@ package rooms_and_exams;
 import java.util.HashSet;
 import java.util.Set;
 
+import logicalcollections.LogicalSet;
+
+/**
+ * @invar | getExams().stream().allMatch(exam -> exam != null && exam.getRooms().contains(this))
+ * @invar | getExams() != null
+ */
 public class Room {
 	
+	/**
+	 * @invar | room != null
+	 * @invar | exams.stream().allMatch(exam -> exam != null && exam.getRooms().contains(this))
+	 */
 	private Room room;
+	
+	/**
+	 * @peerObjects
+	 */
 	private Set<Exam> exams;
 	
+	/**
+	 * @creates | result
+	 * @post | result != null
+	 * 
+	 */
 	public Set<Exam> getExams() { 
 		return new HashSet<>(exams);
 	}
 	
+	/**
+	 * @pre | exam != null
+	 * @pre | getExams() != null
+	 * @mutates_properties | this.getExams(), exam.getRooms()
+	 * @post | exam.getRooms().equals(LogicalSet.plus(old(exam.getRooms()), this)) 
+	 */
 	public Set<Exam> addExam(Exam exam) { 
 		if (exams.contains(exam)) {
 			return new HashSet<>(exams);
@@ -22,6 +47,12 @@ public class Room {
 		}
 	}
 	
+	/**
+	 * @pre | getExams() != null
+	 * @pre | exam != null
+	 * @mutates_properties | this.getExams(), exam.getRooms()
+	 * @post | exam.getRooms().equals(LogicalSet.minus(old(exam.getRooms()), this)) 
+	 */
 	public Set<Exam> removeExam(Exam exam) { 
 		if (exams.contains(exam)) {
 			exams.remove(exam);
@@ -31,11 +62,19 @@ public class Room {
 		return new HashSet<>(null);
 	}
 	
+	/**
+	 * @pre | name != null
+	 * @creates | this
+	 */
 	public Room(String name) { 
 		name = room.toString();
 		this.exams = new HashSet<>();
 	}
 	
+	/**
+	 * @inspects | this
+	 * @post | result != null
+	 */
 	public Room getRoom() { 
 		return this.room;
 	}
