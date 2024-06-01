b3097b4f75999edb76723c1f747af1022328a04a
Charalampos Anagnostakis <charalampos.anagnostakis@student.kuleuven.be>
2024-06-01 17:47:04 +0200

    documentation Node

--- Prep_for_exams/src/GraphDiagram/Node.java
+++ Prep_for_exams/src/GraphDiagram/Node.java
@@ -3,21 +3,43 @@ package GraphDiagram;
 import java.util.HashSet;
 import java.util.Set;
 
-sealed abstract class Node permits RectangularNode, CircularNode{
+/**
+ * @invar | getOutgoingArcs().stream().allMatch(arc -> arc != null && arc.getSourceNode() == this)
+ * @invar | getIncomingArcs().stream().allMatch(arc -> arc != null && arc.getTargetNode() == this)
+ */
+public sealed abstract class Node permits RectangularNode, CircularNode{
 	
 	/**
 	 * @invar | outgoingArcs != null && incomingArcs != null
+	 * @invar | outgoingArcs.stream().allMatch(arc -> arc != null && arc.sourceNode == this)
+	 * @invar | incomingArcs.stream().allMatch(arc -> arc != null && arc.targetNode == this)
+	 * @peerObjects
+	 * @representationObject
 	 */
 	Set<Arc> outgoingArcs = new HashSet<>();
 	
+	/**
+	 * @peerObjects
+	 * @representationObject
+	 */
 	Set<Arc> incomingArcs = new HashSet<>();
 	
 	public Node() {}
 	
+	/**
+	 * @peerObjects
+	 * @creates | result
+	 * @post | result != null
+	 */
 	public Set<Arc> getOutgoingArcs() {
 		return Set.copyOf(outgoingArcs);
 	}
 	
+	/**
+	 * @peerObjects
+	 * @creates | result
+	 * @post | result != null
+	 */
 	public Set<Arc> getIncomingArcs() {
 		return Set.copyOf(incomingArcs);
 	}
