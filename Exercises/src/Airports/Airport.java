5ab01170fce5684f6f9564fc256da9ee2556c88d
Charalampos Anagnostakis <charalampos.anagnostakis@student.kuleuven.be>
2024-06-08 19:51:22 +0200

    Fully implemented airport

--- Exercises/src/Airports/Airport.java
+++ Exercises/src/Airports/Airport.java
@@ -3,21 +3,55 @@ package Airports;
 import java.util.HashSet;
 import java.util.Set;
 
+import logicalcollections.LogicalSet;
+
+/**
+ * @invar | getConnected().stream().allMatch(a -> a != null && a.getConnected().contains(this))
+ */
 public class Airport {
 	
+	/**
+	 * @invar | Airports != null
+	 * @invar | Airports.stream().allMatch(a -> a != null && a.getConnected().contains(this))
+	 * 
+	 * @representationObject 
+	 * @peerObjects
+	 */
 	private Set<Airport> Airports = new HashSet<>();
 	
+	/**
+	 * @creates | result
+	 * 
+	 * @peerObjects
+	 * 
+	 * @post | result != null
+	 * @post | result.stream().allMatch(a -> a != null)
+	 */
 	public Set<Airport> getConnected(){
 		return Set.copyOf(Airports);
 	}
 	
 	public Airport() {}
 	
+	
+	/**
+	 * @pre | other != null
+	 * @mutates_properties | this.getConnected(), other.getConnected()
+	 * @post | this.getConnected().equals(LogicalSet.plus(old(this.getConnected()), other))
+	 * @post | other.getConnected().equals(LogicalSet.plus(old(other.getConnected()), this))
+	 */
 	public void connectedWith(Airport other) {
 		this.getConnected().add(other);
 		other.getConnected().add(this);
 	}
 	
+	/**
+	 * @pre | other != null
+	 * @mutates_properties | this.getConnected(), other.getConnected()
+	 * @post | this.getConnected().equals(LogicalSet.minus(old(this.getConnected()), other))
+	 * @post | other.getConnected().equals(LogicalSet.minus(old(other.getConnected()), this))
+	 * 
+	 */
 	public void disconnectedWith(Airport other) {
 		this.getConnected().remove(other);
 		other.getConnected().remove(this);
--- Exercises/src/Airports/IsConnectedTo.java
+++
@@ -1,5 +0,0 @@
-package Airports;
-
-public class IsConnectedTo {
-
-}
