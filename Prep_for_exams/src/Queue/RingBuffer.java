aae06dcc5d342d38230ff1c3f53f2eb91286bd18
Charalampos Anagnostakis <charalampos.anagnostakis@student.kuleuven.be>
2024-06-10 14:30:12 +0200

    RingBuffer for the Queue

--- Prep_for_exams/src/Queue/RingBuffer.java
+++ Prep_for_exams/src/Queue/RingBuffer.java
@@ -1,59 +1,57 @@
 package Queue;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
-public class RingBuffer extends Queue{
+public class RingBuffer extends Queue {
 	
 	/**
-	 * @invar | 0 <= start
-	 * @invar | start < elements.length
-	 * @invar | 0 <= size
+	 * @invar | elements != null
 	 * @invar | size <= elements.length
-	 * @invar | IntStream.range(0, elements.length).allMatch(i -> (elements[(start + i) % elements.length] != null) == (i < size))
+	 * @invar | start < elements.length
+//	 * @invar | IntStream.range(0, elements.length).allMatch(e -> elements[(start + e) % elements.length] == null || elements[(start + e) % elements.length] == this)
 	 * 
 	 * @representationObject
 	 */
 	private Object[] elements = new Object[3];
-	private int start;
-	private int size;
+	private int start = 0;
+	private int size = 0;
 	
+	
+	public RingBuffer() {}
+
 	@Override
 	public int getSize() {
-		return size;
+		return this.size;
 	}
-	
+
 	@Override
 	public Object[] toArray() {
 		Object[] result = new Object[size];
-		if (start + size <= elements.length)
+		if (start + size <= elements.length) {
 			System.arraycopy(elements, start, result, 0, size);
-		else {
-			int sizeFirstPart = elements.length - start;
-			System.arraycopy(elements, start, result, 0, sizeFirstPart);
-			System.arraycopy(elements, 0, result, sizeFirstPart, size - sizeFirstPart);
+		} else {
+			int firstPart = elements.length - start;
+			System.arraycopy(elements, start, result, 0, firstPart);
+			System.arraycopy(elements, 0, result, firstPart, size - firstPart);
 		}
 		return result;
 	}
-	
-	/**
-	 * @post | getSize() == 0
-	 */
-	public RingBuffer() {}
-	
+
 	@Override
 	public void enqueue(Object element) {
-		if (element == null)
-			throw new IllegalArgumentException("`element` is null");
-		
-		if (elements.length == size) {
-			elements = Arrays.copyOf(toArray(), size * 2);
+		if (element == null) {
+			throw new IllegalArgumentException("The element cannot be null!");
+		}
+		if (size == elements.length) {
+			elements = Arrays.copyOf(toArray(), size*2);
 			start = 0;
 		}
+		elements[(start + size++) % elements.length] = element;
 		
-		elements[(start + size++) % elements.length] = element; 
 	}
-	
+
 	@Override
 	public Object dequeue() {
 		Object result = elements[start];
@@ -62,6 +60,7 @@ public class RingBuffer extends Queue{
 		size--;
 		return result;
 	}
+	
 	
 	
 
